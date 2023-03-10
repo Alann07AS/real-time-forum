74c014f37b827e2e9b5da60ddc80a0b51e04dbfb
Alann <SchnebelenAlann@hotmail.com>
2023-03-10 11:57:11 +0100

    save

--- main.go
+++ main.go
@@ -1,5 +1,15 @@
 package main
 
+import (
+	"net/http"
+)
+
+func greet(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "index.html")
+}
+
 func main() {
-	
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
+	http.HandleFunc("/", greet)
+	http.ListenAndServe(":8080", nil)
 }
